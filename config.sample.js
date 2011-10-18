34f370b9968e18bee895f8df392bc4adc905f155
Anthony Ettinger <anthony@ettinger.name>
2011-10-18 13:49:53 -0700

    remove unused json-line-protocal for digg and meetup. Remove modified config.js and add sample

--- config.sample.js
+++ config.sample.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.port = 8080;
-config.address = 'localhost';
+config.address = '127.0.0.7';
 config.twitter_username = process.env.TWITTER_USERNAME || 'my_username';
 config.twitter_password = process.env.TWITTER_PASSWORD || 'my_password';
 
